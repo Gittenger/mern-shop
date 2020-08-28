00baf67b38e00ee9d6e9bbf9528f2e5e9fd5343a
gittenger <john.pittenger86@gmail.com>
2020-08-27 23:57:25 -0400

    user update and read profile

--- routes/user.js
+++ routes/user.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 
 //import methods
-const { userById } = require("../controllers/user.js");
+const { userById, read, update } = require("../controllers/user.js");
 const { requireSignIn, isAdmin, isAuth } = require("../controllers/auth.js");
 
-router.get("/secret/:userId", requireSignIn, isAdmin, (req, res) => {
-  res.json({
-    user: req.profile,
-  });
+router.get("/secret/:userId", requireSignIn, isAuth, isAdmin, (req, res) => {
+	res.json({
+		user: req.profile,
+	});
 });
+
+router.get("/user/:userId", requireSignIn, isAuth, read);
+router.put("/user/:userId", requireSignIn, isAuth, update);
 
 //anytime param userId is present in URL, add user to req obj
 router.param("userId", userById);

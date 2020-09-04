c169a12b6b7fc35d1c80065bc9c00c84a04e84d1
gittenger <john.pittenger86@gmail.com>
2020-09-03 23:56:11 -0400

    updating dashboard

--- client/src/user/userDashboard.jsx
+++ client/src/user/userDashboard.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import Layout from "../core/Layout";
+import { auth } from "../utils/utils-index";
+
+const { checkAuthToken } = auth;
 
 const Dashboard = () => {
+	const authState = checkAuthToken();
+
 	return (
-		<Layout title="Dashboard" description="User dashboard">
-			...content
+		<Layout title="Dashboard" description="User dashboard" className="container">
+			<div className="card mb-5">
+				<h3 className="card-header">User information</h3>
+				<ul className="list-group">
+					<li className="list-group-item">name</li>
+					<li className="list-group-item">email</li>
+					<li className="list-group-item">role</li>
+				</ul>
+
+				<div className="card">
+					<h3 className="card-header">Purchase history</h3>
+					<ul className="list-group">
+						<li className="list-group-item">history</li>
+					</ul>
+				</div>
+			</div>
 		</Layout>
 	);
 };
